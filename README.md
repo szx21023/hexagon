a4cd26a967bdedf93a9e7eb17182157e58a3d726
herry <herry@program.com.tw>
2021-02-05 17:48:18 +0800

    refactor: drop duplicates code

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # hexagon
+
+grid(start, radius, width, height)
+起始座標(x軸, y軸)(公尺)
+半徑
+水平網格數
+垂直網格數
--- hexagon.py
+++ hexagon.py
@@ -1,5 +1,4 @@
-import geopandas
-from geopandas import GeoSeries
+import geopandas as gpd
 from shapely.geometry import Polygon
 from math import sin, cos, pi
 
@@ -21,11 +20,6 @@ class Hexagon():
         y = self.center.y
         r = self.radius
         return [Point(x + r * cos(pi*i/3), y + r * sin(pi*i/3)).get_coor() for i in range(6)]
-
-# 起始座標(x軸, y軸)(公尺)
-# 半徑
-# 水平網格數
-# 垂直網格數
     
 class Grid():
     def __init__(self, start, radius, width, height):
@@ -35,36 +29,24 @@ class Grid():
         self.height = height
         
     def grid_map(self):
-        start = self.start
-        radius = self.radius
-        width = self.width
-        height = self.height
-
         hexagon_l = []
-        for h in range(height):
-            for w in range(width):
-                if w == 0:
-                    x = start[0]
-                    y = start[1] - h*2*radius*sin(pi*1/3)
-
-                elif w % 2 == 0:
-                    x = start[0] + radius*(w*1.5)
-                    y = start[1] - h*2*radius*sin(pi*1/3)
+        for h in range(self.height):
+            for w in range(self.width):
+                if w % 2 == 0:
+                    x = self.start[0] + self.radius*(w*1.5)
+                    y = self.start[1] - h*2*self.radius*sin(pi*1/3)
 
                 else:
-                    x = start[0] + radius * cos(pi*5/3) + radius*((w-1)*1.5+1)
-                    y = start[1] + radius * sin(pi*5/3) - h*2*radius*sin(pi*1/3)
+                    x = self.start[0] + self.radius * cos(pi*5/3) + self.radius*((w-1)*1.5+1)
+                    y = self.start[1] + self.radius * sin(pi*5/3) - h*2*self.radius*sin(pi*1/3)
 
                 center = Point(x, y)
-                hexagon_l.append(Hexagon(center, radius))
+                hexagon_l.append(Hexagon(center, self.radius))
         return hexagon_l
 
     def to_gdf(self):
         Polygon_l = [Polygon(hexgon.get_vertexs()) for hexgon in self.grid_map()]
-        geometry = GeoSeries(Polygon_l)
-        id_l = [i for i in range(len(Polygon_l))]
-
-        gdf = geopandas.GeoDataFrame(id_l, geometry=geometry).rename(columns={0: 'id'})
-        gdf.crs = {'init' :'epsg:3826'}
-        gdf = gdf.to_crs({'init': 'epsg:4326'})
-        return gdf
+        geometry = gpd.GeoSeries(Polygon_l)
+        data = {'id': [i for i in range(len(Polygon_l))]}
+        crs = {'init' :'epsg:3826'}
+        return gpd.GeoDataFrame(data, crs=crs, geometry=geometry)
